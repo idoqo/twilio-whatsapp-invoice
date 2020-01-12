b84b4017d7e343eafe77bc811f99e88ff3e59977
idoko <radpangolin@gmail.com>
2020-01-12 22:53:04 +0100

    yay! sent our first whatsapp message

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,9 +7,11 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Log;
 use Stripe\Charge;
 use Stripe\Customer;
 use Stripe\Stripe;
+use Twilio\Rest\Client;
 
 class Controller extends BaseController
 {
@@ -28,6 +30,14 @@ class Controller extends BaseController
         $data = [
             'product' => $product
         ];
+        $twilio = new Client(env('TWILIO_ACCOUNT_SID'), env('TWILIO_AUTH_TOKEN'));
+        $message = $twilio->messages->create(
+            "whatsapp:+2349074919559", [
+                "from" => "whatsapp:+14155238886",
+                "body" => "Vroooom!"
+            ]
+        );
+        Log::info($message->sid);
         return view('checkout', $data);
     }
 
